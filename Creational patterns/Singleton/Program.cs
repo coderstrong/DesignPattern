74fb8e3d6dd255ed2eafc18caa4d9ef74b300b3a
Thao CT <thao31b@gmail.com>
2018-10-25 18:23:43 +0800

    add singleton and update

---
+++ Creational patterns/Singleton/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+            // Create book
+            Logs.Instance.writeLog("hello word");
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Logs
+    {
+        #region SingleTon constructor
+        private Logs() { }
+
+        public static Logs Instance
+        {
+            get
+            {
+                return Nested._instance;
+            }
+        }
+
+        class Nested
+        {
+            static Nested() { }
+            internal volatile static Logs _instance = new Logs();
+        }
+        #endregion
+
+        public void writeLog(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ Structural patterns/README.md
@@ -0,0 +1,1 @@
+In Software Engineering, Structural Design Patterns are Design Patterns that ease the design by identifying a simple way to realize relationships between entities.
